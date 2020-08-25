fdcb8bed082ed3e314c156687e7cdc874fce7c87
Patryk Makowiak <patryk.makowiak@gmail.com>
2020-08-25 17:48:44 +0200

    Add material-ui, eslint

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  settings: {
+    "import/resolver": {
+      "node": {
+        "extensions": [".js", ".jsx", ".ts", ".tsx"]
+      }
+    }
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'linebreak-style': ["error", "windows"],
+    'react/jsx-filename-extension': [
+      2,
+      { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+};
