e7b66a5e93a28d717652438d2a4961bdaa6e5990
Patryk Makowiak <patryk.makowiak@gmail.com>
2020-08-26 19:28:24 +0200

    Add starshipsSlice and peopleSlice

---
+++ src/constants/url.ts
@@ -0,0 +1,2 @@
+export const peopleUrl = 'http://swapi.dev/api/people';
+export const starshipsUrl = 'http://swapi.dev/api/starships';
--- src/views/Game/Game.tsx
+++ src/views/Game/Game.tsx
@@ -1,34 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
+import { useDispatch } from 'react-redux';
 
+import { fetchPeople } from '../../app/peopleSlice';
+import { fetchStarships } from '../../app/starshipsSlice';
 import PersonCard from '../../components/PersonCard/PersonCard';
 import ScoreCounter from '../../components/ScoreCounter/ScoreCounter';
 
-const Game = () => (
-  <Grid container>
-    <Grid xs={6}>
-      <PersonCard
-        name="C-3PO"
-        eyeColor="yellow"
-        hairColor="n/a"
-        mass="75"
-        height="167"
-        skinColor="gold"
-      />
-      <ScoreCounter value={3} />
+const Game = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPeople());
+    dispatch(fetchStarships());
+  }, [dispatch]);
+
+  return (
+    <Grid container>
+      <Grid item xs={6}>
+        <PersonCard
+          name="C-3PO"
+          eyeColor="yellow"
+          hairColor="n/a"
+          mass="75"
+          height="167"
+          skinColor="gold"
+        />
+        <ScoreCounter value={3} />
+      </Grid>
+      <Grid item xs={6}>
+        <PersonCard
+          name="C-3PO"
+          eyeColor="yellow"
+          hairColor="n/a"
+          mass="75"
+          height="167"
+          skinColor="gold"
+        />
+        <ScoreCounter value={4} />
+      </Grid>
     </Grid>
-    <Grid xs={6}>
-      <PersonCard
-        name="C-3PO"
-        eyeColor="yellow"
-        hairColor="n/a"
-        mass="75"
-        height="167"
-        skinColor="gold"
-      />
-      <ScoreCounter value={4} />
-    </Grid>
-  </Grid>
-);
+  );
+};
 
 export default Game;
